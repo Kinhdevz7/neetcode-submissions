5803ce69a193c408e03fc58d55d900602b6e82c6
Kinhdevz7 <kinhngco@gmail.com>
2026-04-22 17:58:53 +0800

    Add: python-parse-input - submission-0

---
+++ Python For Beginners/python-parse-input/submission-0.py
@@ -0,0 +1,9 @@
+from typing import List
+
+def read_integers() -> List[int]:
+    user_input = input()
+    int_list = user_input.split(",")
+# do not modify the code below
+print(read_integers())
+print(read_integers())
+print(read_integers())
