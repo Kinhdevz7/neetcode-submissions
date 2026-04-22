91701412acd111bd20a932e8e1aef4438edd1d00
Kinhdevz7 <kinhngco@gmail.com>
2026-04-22 23:02:34 +0800

    Add: python-object-methods - submission-4

---
+++ Python OOP/python-object-methods/submission-4.py
@@ -0,0 +1,23 @@
+class Pet:
+    def __init__(self, name: str, species: str, hunger: int, energy: int):
+        self.name = name
+        self.species = species
+        self.hunger = hunger
+        self.energy = energy
+    
+# Don't modify the above code
+    def feed(self):
+        self.hunger -= 1
+        print(f"{self.name} has been fed.")
+        print(f"{self.name}'s hunger level: {self.hunger}")
+
+# TODO: Create a pet named "Whiskers" that is a species of 'cat' with hunger level 6 and energy level 8
+whiskers = Pet("Whiskers", "cat", 6,8)
+my_pet = Pet("Fluffy", "cat", 5, 8)
+
+my_pet.feed()
+my_pet.feed()
+my_pet.feed()
+
+# Don't modify the following code
+# print(f"{whiskers.name} ({whiskers.species}) - Hunger: {whiskers.hunger}, Energy: {whiskers.energy}") 
