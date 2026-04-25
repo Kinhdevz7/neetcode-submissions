747e88e65cd66a3791dfedc424a3b95c0ada6a0a
Kinhdevz7 <kinhngco@gmail.com>
2026-04-26 01:18:12 +0800

    Add: python-diamond-problem-and-method-resolution-order - submission-2

---
+++ Python OOP/python-diamond-problem-and-method-resolution-order/submission-2.py
@@ -0,0 +1,21 @@
+class A:
+    def print_method(self) -> None:
+        print("A")
+
+class B(A):
+    def print_method(self) -> None:
+        print("B")
+
+class C(A):
+    def print_method(self) -> None:
+        print("C")
+
+class D(B, C): 
+    pass
+
+# print Method Resolution Order (MRO) to determine which method to call when dealing with multiple inheritance.
+# print(D.__mro__)
+
+# Do not change the code below
+d = D()
+d.print_method()
