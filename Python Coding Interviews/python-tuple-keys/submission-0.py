9e44a6c9e3919b9bf23787bfd1f27ddebdd1808d
Kinhdevz7 <kinhngco@gmail.com>
2026-05-02 18:22:10 +0800

    Add: python-tuple-keys - submission-0

---
+++ Python Coding Interviews/python-tuple-keys/submission-0.py
@@ -0,0 +1,29 @@
+from typing import List, Set, Tuple
+
+
+def grid_to_set(grid: List[List[int]]) -> Set[Tuple[int, int]]:
+    set_output = set()
+    for row_index, row in enumerate(grid):
+        # print(f"row_index: {row_index} , row: {row}")
+        for col_index, item in enumerate(row):
+            if item == 1:
+                set_output.add((row_index, col_index))
+    return set_output
+
+# do not modify below this line
+
+output1 = grid_to_set([[1, 0, 1], [0, 1, 0], [1, 0, 1]])
+print(type(output1))
+print(sorted(list(output1)))
+      
+output2 = grid_to_set([[1, 0, 0], [0, 0, 0]])
+print(type(output2))
+print(sorted(list(output2)))
+
+output3 = grid_to_set([[1, 1, 1], [1, 1, 1]])
+print(type(output3))
+print(sorted(list(output3)))
+
+output4 = grid_to_set([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+print(type(output4))
+print(sorted(list(output4)))
